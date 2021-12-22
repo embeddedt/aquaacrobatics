4e2999b9ed716348e2d6aaded0d1218853b48b9a
Themba Dube <embeddedthemba@gmail.com>
2021-12-22 11:58:34 -0500

    Fix running in dev

--- src/main/java/com/fuzs/aquaacrobatics/core/AquaAcrobaticsCore.java
+++ src/main/java/com/fuzs/aquaacrobatics/core/AquaAcrobaticsCore.java
@@ -3,11 +3,18 @@ package com.fuzs.aquaacrobatics.core;
 import com.fuzs.aquaacrobatics.AquaAcrobatics;
 import com.fuzs.aquaacrobatics.client.handler.NoMixinHandler;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.relauncher.CoreModManager;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
 
 import javax.annotation.Nullable;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.CodeSource;
 import java.util.Map;
 
 @SuppressWarnings("unused")
@@ -22,7 +29,32 @@ public class AquaAcrobaticsCore implements IFMLLoadingPlugin {
 
     private static boolean isLoaded;
     private static boolean isScreenRegistered;
-
+    
+    public AquaAcrobaticsCore() {
+        try {
+            Class.forName("org.spongepowered.asm.launch.MixinTweaker");
+        } catch(ClassNotFoundException e) {
+            throw new RuntimeException("No instance of Mixin framework detected. Unable to proceed load.", e);
+        }
+        
+        MixinBootstrap.init();
+        Mixins.addConfiguration("META-INF/mixins." + AquaAcrobaticsCore.MODID + ".json");
+        isLoaded = true;
+        
+        CodeSource codeSource = this.getClass().getProtectionDomain().getCodeSource();
+        if (codeSource != null) {
+            URL location = codeSource.getLocation();
+            try {
+                File file = new File(location.toURI());
+                if (file.isFile()) {
+                    CoreModManager.getReparseableCoremods().remove(file.getName());
+                }
+            } catch (URISyntaxException ignored) {}
+        } else {
+            AquaAcrobaticsCore.LOGGER.warn("No CodeSource, if this is not a development environment we might run into problems!");
+            AquaAcrobaticsCore.LOGGER.warn(this.getClass().getProtectionDomain());
+        }
+    }
     @Override
     public String[] getASMTransformerClass() {
 
@@ -39,28 +71,14 @@ public class AquaAcrobaticsCore implements IFMLLoadingPlugin {
     @Nullable
     @Override
     public String getSetupClass() {
-
-        try {
-
-            if (Class.forName("org.spongepowered.asm.launch.MixinTweaker") != null) {
-
-                isLoaded = true;
-                AquaAcrobaticsCore.LOGGER.info("Found valid Mixin framework. Proceeding to load.");
-                return AquaAcrobaticsSetupHook.class.getName();
-            }
-        } catch (ClassNotFoundException ignored) {
-
-        }
-
-        AquaAcrobaticsCore.LOGGER.error("No instance of Mixin framework detected. Unable to proceed load.");
         return null;
     }
 
     @Override
     public void injectData(Map<String, Object> data) {
-
     }
 
+    
     @Override
     public String getAccessTransformerClass() {
 
