9623d39bfe1efc06e56659fd4985abc404dd844d
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-02-11 19:29:45 -0500

    Make Dynamic Trees fix optional

--- src/main/java/com/fuzs/aquaacrobatics/core/dynamictrees/mixin/client/ModelRootyWaterMixin.java
+++ src/main/java/com/fuzs/aquaacrobatics/core/dynamictrees/mixin/client/ModelRootyWaterMixin.java
@@ -7,11 +7,11 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(ModelRootyWater.class)
 public class ModelRootyWaterMixin {
-    @ModifyConstant(method = "<init>", constant = @Constant(stringValue = "minecraft:blocks/water_still"))
+    @ModifyConstant(method = "<init>", constant = @Constant(stringValue = "minecraft:blocks/water_still"), require = 0)
     private String getStillTexture(String original) {
         return "aquaacrobatics:blocks/water_still";
     }
-    @ModifyConstant(method = "<init>", constant = @Constant(stringValue = "minecraft:blocks/water_flow"))
+    @ModifyConstant(method = "<init>", constant = @Constant(stringValue = "minecraft:blocks/water_flow"), require = 0)
     private String getFlowingTexture(String original) {
         return "aquaacrobatics:blocks/water_flow";
     }
