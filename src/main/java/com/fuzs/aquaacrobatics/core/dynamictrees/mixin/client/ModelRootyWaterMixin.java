f473ab0155eb92fecc9be5d18c22759170384c18
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-02-11 19:47:45 -0500

    Handle newWaterColors config for Dynamic Trees

--- src/main/java/com/fuzs/aquaacrobatics/core/dynamictrees/mixin/client/ModelRootyWaterMixin.java
+++ src/main/java/com/fuzs/aquaacrobatics/core/dynamictrees/mixin/client/ModelRootyWaterMixin.java
@@ -1,6 +1,7 @@
 package com.fuzs.aquaacrobatics.core.dynamictrees.mixin.client;
 
 import com.ferreusveritas.dynamictrees.models.ModelRootyWater;
+import com.fuzs.aquaacrobatics.config.ConfigHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
@@ -9,10 +10,16 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 public class ModelRootyWaterMixin {
     @ModifyConstant(method = "<init>", constant = @Constant(stringValue = "minecraft:blocks/water_still"), require = 0)
     private String getStillTexture(String original) {
-        return "aquaacrobatics:blocks/water_still";
+        if(ConfigHandler.BlocksConfig.newWaterColors)
+            return "aquaacrobatics:blocks/water_still";
+        else
+            return original;
     }
     @ModifyConstant(method = "<init>", constant = @Constant(stringValue = "minecraft:blocks/water_flow"), require = 0)
     private String getFlowingTexture(String original) {
-        return "aquaacrobatics:blocks/water_flow";
+        if(ConfigHandler.BlocksConfig.newWaterColors)
+            return "aquaacrobatics:blocks/water_flow";
+        else
+            return original;
     }
 }
