48ba35fa75bff5ea725ab5b631eafe1af9b5f034
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-05-11 13:16:57 -0400

    Fix JourneyMap compat

--- src/main/java/com/fuzs/aquaacrobatics/core/mixin/BiomeMixin.java
+++ src/main/java/com/fuzs/aquaacrobatics/core/mixin/BiomeMixin.java
@@ -1,9 +1,13 @@
 package com.fuzs.aquaacrobatics.core.mixin;
 
 import com.fuzs.aquaacrobatics.biome.BiomeWaterFogColors;
+import com.fuzs.aquaacrobatics.config.ConfigHandler;
 import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Biome.class)
 public abstract class BiomeMixin {
@@ -12,6 +16,17 @@ public abstract class BiomeMixin {
     /* For OptiFine */
     @SuppressWarnings("unused")
     public int aqua$waterColorMultiplier() {
-        return BiomeWaterFogColors.getWaterColorForBiome((Biome)(Object)this, getWaterColorMultiplier());
+        if(ConfigHandler.BlocksConfig.newWaterColors) {
+            /* We might call getWaterColorForBiome twice, but it's fine because it caches after the first call */
+            return BiomeWaterFogColors.getWaterColorForBiome((Biome) (Object) this, getWaterColorMultiplier());
+        } else
+            return getWaterColorMultiplier();
+    }
+
+    @Inject(method = "getWaterColorMultiplier", at = @At("TAIL"), remap = false, cancellable = true)
+    private void forceNewColor(CallbackInfoReturnable<Integer> cir) {
+        if(ConfigHandler.BlocksConfig.newWaterColors) {
+            cir.setReturnValue(BiomeWaterFogColors.getWaterColorForBiome((Biome)(Object)this, cir.getReturnValue()));
+        }
     }
 }
