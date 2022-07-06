62d8381a0f891fc3f93ef0f1b28c5a212afd5089
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-07-06 10:38:24 -0400

    Disable JourneyMap color patch when Fluidlogged API is installed

--- src/main/java/com/fuzs/aquaacrobatics/core/journeymap55/mixin/client/VanillaBlockSpriteProxyMixin.java
+++ src/main/java/com/fuzs/aquaacrobatics/core/journeymap55/mixin/client/VanillaBlockSpriteProxyMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.client.FMLClientHandler;
+import net.minecraftforge.fml.common.Loader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -29,7 +30,7 @@ public class VanillaBlockSpriteProxyMixin {
     )
     private void getSprites(BlockMD blockMD, CallbackInfoReturnable<Collection<ColoredSprite>> cir) {
         Block block = blockMD.getBlockState().getBlock();
-        if(ConfigHandler.BlocksConfig.newWaterColors && (block == Blocks.WATER || block == Blocks.FLOWING_WATER)) {
+        if(ConfigHandler.BlocksConfig.newWaterColors && (block == Blocks.WATER || block == Blocks.FLOWING_WATER) && !Loader.isModLoaded("fluidlogged_api")) {
             TextureAtlasSprite tas = FMLClientHandler.instance().getClient().getTextureMapBlocks().getAtlasSprite("aquaacrobatics:blocks/water_still");
             cir.setReturnValue(Collections.singletonList(new ColoredSprite(tas, null)));
         }
